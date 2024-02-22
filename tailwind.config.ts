cc3de5fcd1ce7d6667e96e061016f59c0cd54199
Moisés <moisesnetored@gmail.com>
2024-02-22 11:38:55 -0400

    feat: form fields

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,7 @@ const config: Config = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
     },
   },
